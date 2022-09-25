be4cf09d6c57718fa18fafb6c31dd9b922cc0d86
Andrey I <iaa@bork.ru>
2022-09-25 20:10:55 +0200

    initial

---
+++ HeartstoneAssessment/HeartstoneAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartstoneAssessmentApp.swift
+//  HeartstoneAssessment
+//
+//  Created by atme on 25/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct HeartstoneAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
